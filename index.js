cdc59417beacabe7a9a48a90f8b29a2acb03e177
John K. Speck <speckworks@gmail.com>
2020-06-05 22:42:23 +0000

    added potential solution tweak to comments below

--- index.js
+++ index.js
@@ -33,3 +33,29 @@ commonYears(arr1);
 //test case B
 let arr2 = [[1901,1903],[1902,1904],[1903,1905],[1899,1905]]
 commonYears(arr2);
+
+
+//
+// Analyze this code to figure out how to return only the most common years.
+// function modeArray(array) {
+//   if (array.length == 0) return null;
+//   var modeMap = {},
+//     maxCount = 1,
+//     modes = [];
+
+//   for (var i = 0; i < array.length; i++) {
+//     var el = array[i];
+
+//     if (modeMap[el] == null) modeMap[el] = 1;
+//     else modeMap[el]++;
+
+//     if (modeMap[el] > maxCount) {
+//       modes = [el];
+//       maxCount = modeMap[el];
+//     } else if (modeMap[el] == maxCount) {
+//       modes.push(el);
+//       maxCount = modeMap[el];
+//     }
+//   }
+//   return modes;
+// }
