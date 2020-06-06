f9738c04c6237504286edeaafa553fb13420cac7
John K. Speck <speckworks@gmail.com>
2020-06-06 18:14:16 +0000

    replaced let

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 function commonYears(arr){
   //first I make a copy of the Arr (so as not to mutate original array) and flatten the array to make it easier to iterate through given values
-  let allYears = arr.slice().flat(), list = [], counts={};
+  let allYears = arr.slice().flat(), list = [], counts = {};
   //next, I create a list that contains all the values in the ranges given
   for(let i = 0; i < allYears.length; i++){
     //this for loop creates ranges from the ranges and should keep time complexity within O(n), because it is only analyzing value at index and it's neighbor n+1
@@ -15,8 +15,8 @@ function commonYears(arr){
   //i create a copy of the array of all ranges so as not to mutate the original list
   let rangeList = list.slice();
   //now we have to get the counts for each year across the ranges given
-  for (var k=0;k<rangeList.length;k++) {
-    var item = rangeList[k];
+  for (let k = 0; k < rangeList.length; k++) {
+    let item = rangeList[k];
     //analyse each item of the rangeList, assign it to a key of the  "counts object" and assign a value that represents it's key.
     counts[item] = counts[item] >= 1 ? counts[item] + 1 : 1;
   }
@@ -33,7 +33,7 @@ const getMax = object => {
     console.log(getMax(counts)
       );
 }
- let arr1 = [[1910, 1950], [1900, 1951], [1945, 2000]]//return [1945, 1946, 1947, 1948, 1949, 1950]
+let arr1 = [[1910, 1950], [1900, 1951], [1945, 2000]]//return [1945, 1946, 1947, 1948, 1949, 1950]
 commonYears(arr1);
 //test case B
 let arr2 = [[1901,1903],[1902,1904],[1903,1905],[1899,1905]] //return 1903 test for 4 arrays given 
